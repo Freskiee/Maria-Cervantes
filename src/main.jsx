2685aa85f389adccfa51f452fd5c074f2669ea92
Freskiee <freska_96@hotmail.com>
2026-04-15 17:28:22 -0600

    aaa

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,10 @@ if ("scrollRestoration" in window.history) {
   window.history.scrollRestoration = "manual";
 }
 
+if (window.location.hash) {
+  window.history.replaceState(null, '', window.location.pathname + window.location.search);
+}
+
 window.scrollTo(0, 0);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
